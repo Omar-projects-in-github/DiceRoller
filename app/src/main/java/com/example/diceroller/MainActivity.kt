050d6600ea61a5d947dbe13fa0847f6f533aec21
Omar-projects-in-github <200107057@stu.sdu.edu.kz>
2022-09-28 01:13:10 +0600

    diceroller_6.4

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -11,9 +11,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
-            val toast = Toast.makeText(this, "Dice Rolled!",
-            Toast.LENGTH_SHORT)
-            toast.show()
+            val resultTextView: TextView = findViewById(R.id.textView)
+            resultTextView.text = "6"
         }
+    }
+}
+
+class Dice(val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
